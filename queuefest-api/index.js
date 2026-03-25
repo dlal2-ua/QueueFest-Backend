d1396f859012ba63f9987a5f6af17b69669834d5
asts3-ua <asts3-ua@alu.ua.es>
2026-03-25 17:49:15 +0000

    cambios

--- queuefest-api/index.js
+++ queuefest-api/index.js
@@ -54,7 +54,7 @@ async function initDB() {
 }
 initDB();
 
-// Middleware para verificar token
+// Middleware para verificar tokenn
 const auth = async (req, res, next) => {
   const token = req.headers.authorization?.split(' ')[1];
   if (!token) return res.status(401).json({ error: 'No autorizado' });
